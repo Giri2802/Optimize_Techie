5e8c7c69fd1fc3f57fd200ffd0c456b109f41eda
gpachipulusu <gpachipulusu@miraclesoft.com>
2024-09-17 19:02:07 +0530

    Initial Commit

---
+++ src/app/dashboard/solid-footer/solid-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solid-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './solid-footer.component.html',
+  styleUrl: './solid-footer.component.scss'
+})
+export class SolidFooterComponent {
+
+}
